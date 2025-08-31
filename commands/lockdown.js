639c0f94cd1a2585c67af72ae7c64ebc2c442f13
NeonOfficialYouTube <neonbirdofficialYT@gmail.com>
2025-08-31 13:13:44 +0100

    Create lockdown.js

---
+++ commands/lockdown.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('lockdown')
+        .setDescription('Lock or unlock the entire server')
+        .addBooleanOption(option =>
+            option.setName('lock')
+                  .setDescription('true to lockdown, false to unlock')
+                  .setRequired(true)
+        ),
+    async execute(interaction) {
+        if (!interaction.member.permissions.has('Administrator')) {
+            return interaction.reply({ content: '❌ You need to be an admin to use this.', ephemeral: true });
+        }
+
+        const lock = interaction.options.getBoolean('lock');
+        const everyoneRole = interaction.guild.roles.everyone;
+
+        try {
+            // Loop through all channels
+            interaction.guild.channels.cache.forEach(async channel => {
+                await channel.permissionOverwrites.edit(everyoneRole, { SEND_MESSAGES: !lock });
+            });
+
+            await interaction.reply({
+                content: lock ? '🔒 Server is now in lockdown!' : '🔓 Server has been unlocked!',
+                ephemeral: false
+            });
+        } catch (err) {
+            console.error(err);
+            interaction.reply({ content: '❌ An error occurred while changing permissions.', ephemeral: true });
+        }
+    }
+};
