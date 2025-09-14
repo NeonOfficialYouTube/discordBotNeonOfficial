9a3ed65113cf538f6327422a0a7b87515c7e7499
NeonOfficialYouTube <neonbirdofficialYT@gmail.com>
2025-09-14 18:04:38 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,21 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+// List of allowed user IDs
+const allowedUsers = ['1345050725637685288', '1320938370586902609'];
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('say')
         .setDescription('Bot sends a message in the channel')
-        .addStringOption(option => 
+        .addStringOption(option =>
             option.setName('message')
                 .setDescription('The message to send')
                 .setRequired(true)
         ),
     async execute(interaction) {
+        // Check if user is allowed
+        if (!allowedUsers.includes(interaction.user.id)) {
+            return interaction.reply({ content: "You can't use this command!", ephemeral: true });
+        }
+
         const text = interaction.options.getString('message');
-
-        // Send the message in the same channel
-        await interaction.channel.send(text);
-
-        // Optional: reply to the user that it worked (ephemeral = only they see it)
-        await interaction.reply({ content: 'Message sent!', ephemeral: true });
+        await interaction.channel.send(text); // sends normal message
+        await interaction.reply({ content: 'Message sent!', ephemeral: true }); // private confirmation
     },
 };
